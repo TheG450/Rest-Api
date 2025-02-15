9bec91b30062cfefaa57a16a7e6cf77312606a81
TheG450 <120777134+TheG450@users.noreply.github.com>
2025-02-16 00:39:30 +0700

    Update userRoute.js

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { Ready, getUsers, redeemKey, getKey, getUserMap, getAccess, resetHwid } = require('../controllers/usersController')
+const { Ready, getUsers, redeemKey, getKey, getUserMap, getAccess, resetHwid, deleteUser } = require('../controllers/usersController')
 
 
 router.get('/', Ready);
@@ -10,6 +10,7 @@ router.get('/getkey', getKey);
 router.get('/getmaps', getUserMap);
 router.get('/getaccess', getAccess);
 router.post('/resethwid', resetHwid);
+router.post('/deleteuser', deleteUser);
 //router.put('/edit-user/:username', updateUser);
 
 module.exports = router;
